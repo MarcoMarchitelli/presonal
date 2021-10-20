6be652131251e8604203541042a392537f5ea7e4
Marco Marchitelli <35488737+MarcoMarchitelli@users.noreply.github.com>
2021-10-20 22:08:57 +0200

    idk

--- main.js
+++ main.js
@@ -5,31 +5,16 @@
  const deirinUnderlay = document.getElementById("deirinUnderlay");
 
  /*events*/
- deirinButton.addEventListener("mouseenter", DeirinMouseEnter);
+ deirinButton.addEventListener("mouseenter");
  deirinButton.addEventListener("mouseleave", DeirinMouseEnter);
  deirinButton.addEventListener("mousedown", aOverlay_Down);
- 
- function aOverlay_Down(){
-    deirinText.style.position = deirinUnderlay.style.position;
+
+ function test(){
+    anime({
+        targets: '.titleText',
+        translateX: -35,
+        translateY: 50,
+        diration: 1000,
+        easing: 'easeOut'
+     });
  }
-
- function animate({timing, draw, duration}) {
-
-    let start = performance.now();
-  
-    requestAnimationFrame(function animate(time) {
-      // timeFraction goes from 0 to 1
-      let timeFraction = (time - start) / duration;
-      if (timeFraction > 1) timeFraction = 1;
-  
-      // calculate the current animation state
-      let progress = timing(timeFraction);
-  
-      draw(progress); // draw it
-  
-      if (timeFraction < 1) {
-        requestAnimationFrame(animate);
-      }
-  
-    });
-  }
