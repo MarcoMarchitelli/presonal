bbb2597f5c1f956e47534f3ee2bd75330a35ec77
Marco Marchitelli <35488737+MarcoMarchitelli@users.noreply.github.com>
2021-11-05 21:40:15 +0100

    elegant js update, category button anims

--- main2.js
+++ main2.js
@@ -4,36 +4,94 @@ function LoadHandler() {
     InitVars();
 }
 
-let bGames;
-let gGames;
-let bGamesClicked = false;
+let games_button;
+let games_underlay;
+let games_grid;
+let gamesClicked = false;
+
+let shaders_button;
+let shaders_underlay;
+let shaders_grid;
+let shadersClicked = false;
 
 function InitVars() {
-    bGames = document.getElementById("gamesButton");
-    bGames.addEventListener("click", GamesClick);
-    gGames = document.getElementById("gamesGrid");
+    games_button = document.getElementById("gamesButton");
+    games_underlay = games_button.lastElementChild;
+    games_grid = document.getElementById("gamesGrid");
+    games_button.addEventListener("click", GamesClick);
+
+    shaders_button = document.getElementById("shadersButton");
+    shaders_underlay = shaders_button.lastElementChild;
+    shaders_grid = document.getElementById("shadersGrid");
+    shaders_button.addEventListener("click", ShadersClick);
 }
 
-function GamesClick() {
-    if (bGamesClicked == false) {
-        gGames.style.display = "auto";
-        aGamesGridShow.restart();
-        bGamesClicked = true;
+function CategoryClick(underlay, content, selectionStatus) {
+    if(selectionStatus == false) {
+        
     }
 }
 
-let aGamesGridShow = anime({
-    targets: '#gamesGrid',
-    opacity: '1',
-    duration: 500,
-    autoplay: false,
-    easing: 'easeOutQuad'
-});
+function GamesClick() {
+    if (gamesClicked == false) {
+        games_grid.style.display = "auto";
 
-let aGamesGridHide = anime({
-    targets: '#gamesGrid',
-    opacity: '0',
-    duration: 500,
-    autoplay: false,
-    easing: 'easeOutQuad'
-});
+        aContentShow(games_grid);
+        aUnderlayShow(games_underlay);
+
+        gamesClicked = true;
+    }
+}
+
+function ShadersClick() {
+    if (shadersClicked == false) {
+        shaders_grid.style.display = "auto";
+
+        aContentShow(shaders_grid);
+        aUnderlayShow(shaders_underlay);
+
+        shadersClicked = true;
+    }
+}
+
+function aUnderlayShow(target) {
+    anime({
+        targets: target.style,
+        opacity: ['0','1'],
+        width: '100%',
+        duration: 250,
+        autoplay: true,
+        easing: 'easeOutQuad'
+    });
+}
+
+function aUnderlayHide(target) {
+    anime({
+        targets: target.style,
+        opacity: ['1','0'],
+        width: '0',
+        duration: 250,
+        autoplay: true,
+        easing: 'easeOutQuad'
+    });
+}
+
+function aContentShow(target) {
+    anime({
+        targets: target.style,
+        opacity: ['0','1'],
+        duration: 250,
+        autoplay: true,
+        easing: 'easeOutQuad'
+    });
+}
+
+function aContentHide(target) {
+    anime({
+        targets: target.style,
+        opacity: ['1','0'],
+        duration: 250,
+        autoplay: true,
+        easing: 'easeOutQuad'
+    });
+}
