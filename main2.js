6776911f86d347b3ea2e1b10d8af8f813af29402
Marco Marchitelli <35488737+MarcoMarchitelli@users.noreply.github.com>
2021-11-06 01:14:20 +0100

    more js

--- main2.js
+++ main2.js
@@ -14,22 +14,60 @@ let shaders_underlay;
 let shaders_grid;
 let shadersClicked = false;
 
+let lastSelected;
+
 function InitVars() {
     games_button = document.getElementById("gamesButton");
     games_underlay = games_button.lastElementChild;
     games_grid = document.getElementById("gamesGrid");
-    games_button.addEventListener("click", GamesClick);
+    games_button.addEventListener("click", _ => CategoryClick(games_button, games_underlay, games_grid, gamesClicked));
 
     shaders_button = document.getElementById("shadersButton");
     shaders_underlay = shaders_button.lastElementChild;
     shaders_grid = document.getElementById("shadersGrid");
-    shaders_button.addEventListener("click", ShadersClick);
+    shaders_button.addEventListener("click", _ => CategoryClick(shaders_button, shaders_underlay, shaders_grid, shadersClicked));
 }
 
-function CategoryClick(underlay, content, selectionStatus) {
-    if(selectionStatus == false) {
-        
+function CategoryClick(button, underlay, content, selectionStatus) {
+    DeselectAll();
+    if (selectionStatus == false) {
+        Select(button, content, underlay, selectionStatus);
     }
+}
+
+function DeselectAll() {
+    Deselect(games_button, games_grid, games_underlay, gamesClicked);
+    Deselect(shaders_button, shaders_grid, shaders_underlay, shadersClicked);
+}
+
+function Deselect(button, content, underlay, selectionStatus) {
+    if (selectionStatus == false)
+        return;
+
+    content.style.display = "none";
+
+    aContentHide(content);
+    aUnderlayHide(underlay);
+
+    //attiva button
+    button.style.pointerEvents = "auto";
+
+    selectionStatus = false;
+}
+
+function Select(button, content, underlay, selectionStatus) {
+    if (selectionStatus == true)
+        return;
+
+    content.style.display = "auto";
+
+    aContentShow(content);
+    aUnderlayShow(underlay);
+
+    //disattiva button
+    button.style.pointerEvents = "none";
+
+    selectionStatus = true;
 }
 
 function GamesClick() {
@@ -57,7 +95,7 @@ function ShadersClick() {
 function aUnderlayShow(target) {
     anime({
         targets: target.style,
-        opacity: ['0','1'],
+        opacity: ['0', '1'],
         width: '100%',
         duration: 250,
         autoplay: true,
@@ -68,7 +106,7 @@ function aUnderlayShow(target) {
 function aUnderlayHide(target) {
     anime({
         targets: target.style,
-        opacity: ['1','0'],
+        opacity: ['1', '0'],
         width: '0',
         duration: 250,
         autoplay: true,
@@ -79,7 +117,7 @@ function aUnderlayHide(target) {
 function aContentShow(target) {
     anime({
         targets: target.style,
-        opacity: ['0','1'],
+        opacity: ['0', '1'],
         duration: 250,
         autoplay: true,
         easing: 'easeOutQuad'
@@ -89,7 +127,7 @@ function aContentShow(target) {
 function aContentHide(target) {
     anime({
         targets: target.style,
-        opacity: ['1','0'],
+        opacity: ['1', '0'],
         duration: 250,
         autoplay: true,
         easing: 'easeOutQuad'
